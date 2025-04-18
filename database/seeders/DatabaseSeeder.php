8741a2746706ea36aadb1472d5dda27b2d06269a
Bagtyyar Rejepov <rejepowbagtyyar05@gmail.com>
2025-04-18 12:24:57 +0500

    added user factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,21 +3,18 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'name' => 'Bagtiyar Rejepov',
             'email' => 'test@example.com',
+            'email_verified_at' => now(),
         ]);
     }
 }
