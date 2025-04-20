e5448b590715a28aa59b26f6b21f5af61ceb56c0
Bagtyyar Rejepov <rejepowbagtyyar05@gmail.com>
2025-04-20 13:57:07 +0500

    added connection between configs and resources

--- database/migrations/2025_04_19_082909_add_config_id_to_resources_table.php
+++ database/migrations/2025_04_19_082909_add_config_id_to_resources_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('resources', function (Blueprint $table) {
-            //
+            $table->foreignId('config_id')->constrained('resource_configs')->restrictOnDelete();
         });
     }
 
