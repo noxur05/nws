767b1163bfe564b9d659e196d509e92224885e7f
Bagtyyar Rejepov <rejepowbagtyyar05@gmail.com>
2025-04-20 18:09:20 +0500

    added bases

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,2 +1,7 @@
-<div class="">Hello WOrld</div>
-<x--form-text-input name="email" type="email" value="{{old('email')}}" id="email" placeholder="Email"/>
+@extends('layouts.index')
+
+@section('title')
+    Hello World
+@endsection
+{{-- <div class="">Hello WOrld</div>
+<x--form-text-input name="email" type="email" value="{{old('email')}}" id="email" placeholder="Email"/> --}}
